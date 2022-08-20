0f8a013a26b23efcbc054eefc3a3cbac9d99047b
kimmingyu <minku4820@gmail.com>
2022-08-20 16:39:50 +0900

    nomad react 7.4 MovieApp Part2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Button from "./Button";
 import styles from "./App.module.css"
 import {useEffect, useState} from "react";
+import Movie from "./Movie";
 
 //function App() {
     //part 1 ToDoList
@@ -92,19 +93,18 @@ function App(){
             {loading ? (
                 <h1>Loading...</h1>
             ) :  (
-                <div>{movies.map((movie) => (
-                    <div key={movie.id}>
-                        <img src={movie.medium_cover_image} />
-                        <h2>{movie.title}</h2>
-                        <p>{movie.summary}</p>
-                        <ul>
-                            {movie.genres.map(g =>(
-                                <li key={g}>{g}</li>
-                            ))}
-                        </ul>
-                    </div>
-                ))}
-                </div>)}
+                <div>
+                    {movies.map((movie) => (
+                        <Movie
+                            key={movie.id}
+                            medium_cover_image={movie.medium_cover_image}
+                            title={movie.title}
+                            summary={movie.summary}
+                            genres={movie.genres}
+                        />
+                    ))}
+                </div>
+            )}
         </div>
     );
 }
