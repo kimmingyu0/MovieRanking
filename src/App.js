2ad4585df03e89e1dab7611164e6a2e49602d116
kimmingyu <minku4820@gmail.com>
2022-08-20 17:19:32 +0900

    nomad react 7.5 React Routers

--- src/App.js
+++ src/App.js
@@ -1,112 +1,18 @@
-import Button from "./Button";
-import styles from "./App.module.css"
-import {useEffect, useState} from "react";
-import Movie from "./Movie";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
-//function App() {
-    //part 1 ToDoList
-    // const [toDo,setToDo] = useState("");
-    // const [toDos,setToDos] = useState([]);
-    // const onChange = (event) => setToDo(event.target.value);
-    // const onSubmit = (event) => {
-    //     event.preventDefault();
-    //     if(toDo===""){
-    //         return;
-    //     }
-    //     setToDos(currentArrays =>[toDo,...toDos]);
-    //     setToDo("");
-    //
-    // }
-    //return (
-        //part 1 ToDoList
-        // <div>
-        //
-        //     <h1>My To DoS ({toDos.length})</h1>
-        //     <form onSubmit={onSubmit}>
-        //         <input
-        //             onChange={onChange}
-        //             value={toDo}
-        //             placeholder="Write your to do..."
-        //         />
-        //         <button>Add To Do</button>
-        //     </form>
-        //     <hr />
-        //     <ul>
-        //         {toDos.map((item, index)=> (
-        //             <li key={index}>{item}</li>
-        //         ))}
-        //     </ul>
-        // </div>
-    //)
-//}
-/*function App() {
-    const [loading,setLoading] = useState(true);
-    const [coins,setCoins] = useState([])
-    useEffect(()=>{
-        fetch("https://api.coinpaprika.com/v1/tickers")
-            .then((response) => response.json())
-            .then((json)=>{
-                setCoins(json);
-                setLoading(false);
-            });
-    },[])
-    return (
-    <div>
-        <h1>TheCoins {loading? "" : `(${coins.length})`}</h1>
-        {loading ? <strong>Loading...</strong> : <select>
-            {
-                coins.map((coin)=>
-                    <option>
-                        {coin.name} ({coin.symbol}) : {coin.quotes.USD.price}USD
-                    </option>)
-            }
-        </select>}
-
-    </div>
-    )
-}*/
 function App(){
-    const [loading,setLoading] = useState(true);
-    const [movies,setMovies] = useState([]);
-    const getMovies = async ()=> {
-        const response = await fetch(
-            `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-        );
-        const json = await  response.json();
-        setMovies(json.data.movies);
-        setLoading(false);
-    }
-    useEffect(()=>{
-        /*fetch(
-            `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-        )
-            .then(response => response.json())
-            .then((json) =>{
-                setMovies(json.data.movies);
-                setLoading(false);
-            });*/
-        getMovies()
-    },[])
-    console.log(movies)
-    return(
-        <div>
-            {loading ? (
-                <h1>Loading...</h1>
-            ) :  (
-                <div>
-                    {movies.map((movie) => (
-                        <Movie
-                            key={movie.id}
-                            medium_cover_image={movie.medium_cover_image}
-                            title={movie.title}
-                            summary={movie.summary}
-                            genres={movie.genres}
-                        />
-                    ))}
-                </div>
-            )}
-        </div>
-    );
+    return <Router>
+        <Switch>
+            <Route path="/movie">
+                <Detail />
+            </Route>
+            <Route path="/">
+                <Home />
+            </Route>
+        </Switch>
+    </Router>
 }
 
 export default App;
